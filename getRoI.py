fixed rotation

getRoI.py
@@ -10,16 +10,20 @@ def getRoi(img, img_mask, debug=False):
 
 	# Obtener la orientacion de la img
 	rotRect = cv.minAreaRect(border)
-	angle = rotRect[-1]
+	angle = rotRect[-1]	
 	if rotRect[1][0] < rotRect[1][1]:
 		angle = angle - 90
 
 	# Rotar la img segun angulo
-	center = (int(height/2), int(width/2))
+	center = (int(width/2), int(height/2))
 	bg_color = tuple(np.int0(np.average(img, axis=(0,1))))
 	rotMtx = cv.getRotationMatrix2D(center, angle, 1.0)
-	result = cv.warpAffine(img, rotMtx, (2*width, 2*height), flags=cv.INTER_LINEAR, borderValue=(int(bg_color[0]), int(bg_color[1]), int(bg_color[2])))
-	result_mask = cv.warpAffine(img_mask, rotMtx, (width, height), flags=cv.INTER_LINEAR)
+	nwidth = int( height*np.abs(rotMtx[0,1]) + width*np.abs(rotMtx[0,0]) )
+	nheight = int( height*np.abs(rotMtx[0,0]) + width*np.abs(rotMtx[0,1]) )
+	rotMtx[0,2] += nwidth/2 - center[0]
+	rotMtx[1,2] += nheight/2 - center[1]
+	result = cv.warpAffine(img, rotMtx, (nwidth, nheight), flags=cv.INTER_LINEAR, borderValue=(int(bg_color[0]), int(bg_color[1]), int(bg_color[2])))
+	result_mask = cv.warpAffine(img_mask, rotMtx, (nwidth, nheight), flags=cv.INTER_LINEAR)
 	th, result_mask = cv.threshold(result_mask, 128, 255, cv.THRESH_BINARY)		# interpolacion genera 'grises'
 
 	# --- Obtener nueva region ya rotada ---
@@ -31,9 +35,10 @@ def getRoi(img, img_mask, debug=False):
 	box = cv.boxPoints(rect)
 	p_ul = [ min(np.int0(box)[:,0]), min(np.int0(box)[:,1])]		# upper-left
 	p_br = [ max(np.int0(box)[:,0]), max(np.int0(box)[:,1])]		# bottom-right
-
+	
 	#cv.drawContours(result, [np.int0(box)], 0, (0,0,255),2)
 	#cv.imshow('asdasd',result)
+	#cv.waitKey(0)
 
 	first_roi = result[p_ul[1]:p_br[1], p_ul[0]:p_br[0]]
 	first_roi_gray = cv.cvtColor(first_roi, cv.COLOR_BGR2GRAY)
@@ -47,18 +52,20 @@ def getRoi(img, img_mask, debug=False):
 	# Orientacion: True = mira izq. , False = mira der.
 	print(left_area, right_area)
 	look_left = True if left_area >= right_area else False
-
+	
 	# --- Upper fin calculation ---
 	# sliding window
-	for i in np.arange(0.2, 0.5, 0.01):
+	for i in np.arange(0.2, 0.5, 0.01):	
 		if look_left:
-			x1 = int(i*fish_length)
+			x1 = int(i*fish_length) 
 			x2 = int((i+0.2)*fish_length)
 		else:
-			x1 = int((1-i-0.2)*fish_length)
+			x1 = int((1-i-0.2)*fish_length) 
 			x2 = int((1-i)*fish_length)
 		fin_roi = first_roi_mask[0:int((p_br[1]-p_ul[1])/2),x1:x2]
-		if (np.where(fin_roi[:,0] == 255)[0][0] < 3) or (np.where(fin_roi[:,-1] == 255)[0][0] < 3):
+		if look_left and (np.where(fin_roi[:,-1] == 255)[0][0] < 2):
+			break
+		elif not look_left and (np.where(fin_roi[:,0] == 255)[0][0] < 2):
 			break
 
 	m1 = 0; b1 = 0; m2 = 0; b2 = 0
@@ -80,15 +87,15 @@ def getRoi(img, img_mask, debug=False):
 
 	# --- Eye coordinates calculation ---
 	c = 0.12		# 0.07 perilla
-	th = 95			#75
+	th = 75
 	if look_left:	# pez mirando a derecha, toma primeros pixeles para buscar ojo
 		a = 0
-		b = int(c*fish_length)
+		b = int(c*fish_length)						
 	else:
 		a = int((1-c)*fish_length)
 		b = fish_length
 
-	eye_roi = first_roi_gray[:, a:b]
+	eye_roi = first_roi_gray[:, a:b]	
 	th, eye = cv.threshold(eye_roi, th, 255, cv.THRESH_BINARY_INV)			# 60 perilla
 	#cv.imshow('as',eye)
 	#cv.waitKey(0)
@@ -142,10 +149,10 @@ def getRoi(img, img_mask, debug=False):
 
 def main():
 	salmon = 'salmon5'
-	scene = '00021'
-	img = cv.imread('frames_salmones/'+salmon+'/original/scene'+scene+'.png')
+	scene = '00001'
+	img = cv.imread(salmon+'/original/scene'+scene+'.png')
 
-	img_mask = cv.imread('frames_salmones/'+salmon+'/binario/scene'+scene+'.png')
+	img_mask = cv.imread(salmon+'/binario/scene'+scene+'.png')
 	img_mask = cv.cvtColor(img_mask, cv.COLOR_BGR2GRAY)		# para dejarlo de 1 canal
 
 	result = getRoi(img, img_mask, debug=True)